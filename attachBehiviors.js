819f2970ce019c8db68412a916f69a079d5c7956
Bruce Anderson <anderson.bruce.b@gmail.com>
2022-10-15 20:38:37 -0400

    0.0.21

--- attachBehiviors.js
+++ attachBehiviors.js
@@ -2,17 +2,19 @@ export async function attachBehiviors(target, mutationSettings) {
     const beHive = target.getRootNode().querySelector('be-hive');
     if (beHive === null)
         return;
-    const beDecoratedProps = Array.from(beHive.children);
-    for (const beDecor of beDecoratedProps) {
-        const el = beDecor;
+    const DEs = Array.from(beHive.children);
+    for (const de of DEs) {
+        const el = de;
         await customElements.whenDefined(el.localName);
-        const matches = Array.from(target.querySelectorAll(`${beDecor.upgrade}[be-${beDecor.ifWantsToBe}],${beDecor.upgrade}[data-be-${beDecor.ifWantsToBe}]`));
+        const upgrade = el.getAttribute('upgrade');
+        const ifWantsToBe = el.getAttribute('if-wants-to-be');
+        const matches = Array.from(target.querySelectorAll(`${upgrade}[be-${ifWantsToBe}],${upgrade}[data-be-${ifWantsToBe}]`));
         for (const match of matches) {
-            const data = match.hasAttribute(`data-be-${beDecor.ifWantsToBe}`) ? 'data-' : '';
-            const attrVal = match.getAttribute(`${data}be-${beDecor.ifWantsToBe}`);
-            match.setAttribute(`${data}is-${beDecor.ifWantsToBe}`, attrVal);
-            match.removeAttribute(`${data}be-${beDecor.ifWantsToBe}`);
-            beDecor.newTargets = [...beDecor.newTargets, match];
+            const data = match.hasAttribute(`data-be-${ifWantsToBe}`) ? 'data-' : '';
+            const attrVal = match.getAttribute(`${data}be-${ifWantsToBe}`);
+            match.setAttribute(`${data}is-${ifWantsToBe}`, attrVal);
+            match.removeAttribute(`${data}be-${ifWantsToBe}`);
+            de.attach(match);
         }
     }
     if (mutationSettings !== undefined) {
--- attachBehiviors.ts
+++ attachBehiviors.ts
@@ -1,19 +1,21 @@
-import {BeDecoratedProps } from 'be-decorated/be-decorated.js';
+import {DEMethods } from 'be-decorated/types.js';
 
 export async  function attachBehiviors(target: Element, mutationSettings?: MutationObserverInit){
     const beHive = (target.getRootNode() as DocumentFragment).querySelector('be-hive');
     if(beHive === null) return;
-    const beDecoratedProps = Array.from(beHive.children) as any as BeDecoratedProps[];
-    for(const beDecor of beDecoratedProps){
-        const el = beDecor as any as Element;
+    const DEs = Array.from(beHive.children) as any as DEMethods[];
+    for(const de of DEs){
+        const el = de as any as Element;
         await customElements.whenDefined(el.localName);
-        const matches = Array.from(target.querySelectorAll(`${beDecor.upgrade}[be-${beDecor.ifWantsToBe}],${beDecor.upgrade}[data-be-${beDecor.ifWantsToBe}]`));
+        const upgrade = el.getAttribute('upgrade');
+        const ifWantsToBe = el.getAttribute('if-wants-to-be');
+        const matches = Array.from(target.querySelectorAll(`${upgrade}[be-${ifWantsToBe}],${upgrade}[data-be-${ifWantsToBe}]`));
         for(const match of matches){
-            const data = match.hasAttribute(`data-be-${beDecor.ifWantsToBe}`) ? 'data-' : '';
-            const attrVal = match.getAttribute(`${data}be-${beDecor.ifWantsToBe}`);
-            match.setAttribute(`${data}is-${beDecor.ifWantsToBe}`, attrVal!);
-            match.removeAttribute(`${data}be-${beDecor.ifWantsToBe}`);
-            beDecor.newTargets = [...beDecor.newTargets, match];
+            const data = match.hasAttribute(`data-be-${ifWantsToBe}`) ? 'data-' : '';
+            const attrVal = match.getAttribute(`${data}be-${ifWantsToBe}`);
+            match.setAttribute(`${data}is-${ifWantsToBe}`, attrVal!);
+            match.removeAttribute(`${data}be-${ifWantsToBe}`);
+            de.attach(match);
         }
     }
     if(mutationSettings !== undefined){
